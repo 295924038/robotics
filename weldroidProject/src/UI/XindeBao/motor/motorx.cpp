b0e2bc4b19c313c8c23e22c815f1c74bf84bd9e8
295924038 <295924038@qq.com>
2019-08-16 21:51:33 +0800

    电机cpp文件增加函数接口，但还未实现

--- weldroidProject/src/UI/XindeBao/motor/motorx.cpp
+++ weldroidProject/src/UI/XindeBao/motor/motorx.cpp
@@ -4,3 +4,74 @@ MotorX::MotorX()
 {
 
 }
+
+//连接设备
+void MotorX::connectDevice()
+{
+
+}
+//断开设备
+void MotorX::disconnectDevice()
+{
+
+}
+//回Home位置
+void MotorX::backHome()
+{
+
+}
+//左移
+void MotorX::moveLeft()
+{
+
+}
+//右移
+void MotorX::moveRight()
+{
+
+}
+//绝对位移
+void MotorX::absoluteMove(double)
+{
+
+}
+//停止移动
+void MotorX::stopMove()
+{
+
+}
+//位置反馈
+void MotorX::getPosition()
+{
+
+}
+//是否失去Home点
+void MotorX::bHomeLost()
+{
+
+}
+//左限位
+void MotorX::leftLimit()
+{
+
+}
+//右限位
+void MotorX::rightLimit()
+{
+
+}
+//报错恢复重置
+void MotorX::reset()
+{
+
+}
+//使能
+void MotorX::enable()
+{
+
+}
+//去使能
+void MotorX::disabled()
+{
+
+}
