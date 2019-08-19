1d0ff0e4a71454b9130814b6c2c1867701d39887
295924038 <295924038@qq.com>
2019-08-19 04:28:59 -0700

    just sync code

--- weldroidProject/src/UI/XindeBao/motor/motor.cpp
+++ weldroidProject/src/UI/XindeBao/motor/motor.cpp
@@ -1,41 +1,334 @@
 #include "motor/motor.h"
+#include "base/config.h"
+#include <iostream>
+#include <thread>
+
+#include <boost/thread.hpp>
+#include <boost/algorithm/hex.hpp>
+
+
+#include "base/core.h"
+#include "base/base.h"
+#include "base/wzerror.h"
+
+using namespace std ;
+using namespace boost::asio;
 
 Motor::Motor()
 {
-
+    connectDevice();
 }
 
 //连接设备
 void Motor::connectDevice()
 {
-
+    std::lock_guard<std::recursive_mutex> lck(_mtx) ;
+    try {
+        if ( !_serialPort_ptr ) {
+            Config config("etc/sys.info") ;
+            _serialPort_ptr.reset( new  serial_port(_service, config.get<std::string>("Motor.serialPort")) );
+            _serialPort_ptr->set_option(serial_port::baud_rate(config.get<int>("Motor.baudRate"))) ;
+            _serialPort_ptr->set_option(serial_port::flow_control(serial_port::flow_control::none)) ;
+            _serialPort_ptr->set_option(serial_port::parity(serial_port::parity::none)) ;
+            _serialPort_ptr->set_option(serial_port::stop_bits(serial_port::stop_bits::one)) ;
+            _serialPort_ptr->set_option(serial_port::character_size(serial_port::character_size(8))) ;
+            boost::asio::async_read( *_serialPort_ptr, boost::asio::buffer(_readBuffer, 2),
+                                     boost::bind( &Motor::recvReply, this, _1, _2 ) ) ;
+            _thService = std::thread([this]() {_service.run();}) ;
+            std::cout << "connect motor success\n" << std::flush ;
+            init();
+        }
+    }
+    catch ( const boost::system::system_error &e ) {
+        std::cout << e.what() << std::endl ;
+    }
+    catch ( const boost::exception &e ) {
+        DUMPERROR(e) ;
+    }
 }
 //断开设备
 void Motor::disconnectDevice()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx) ;
+    if ( _serialPort_ptr ) {
+        if ( _serialPort_ptr->is_open() ) {
+            finish();
+            //this->stop() ;
+            _service.stop() ;
+            _serialPort_ptr->close();
+            _thService.join() ;
+        }
+        _serialPort_ptr = nullptr;
+    }
+}
+void Motor::reconnectDevice()
+{
+    disconnectDevice();
+    connectDevice();
 
 }
 //回Home位置
-void Motor::backHome()
+void Motor::backHomeX()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[2] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE1 ;
+        _cmd[2] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+//回Home位置
+void Motor::backHomeZ()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[2] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE6 ;
+        _cmd[2] = 0x00 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+void Motor::relayOpen()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xEB ;
+        _cmd[2] = 0x01 ;
+        _cmd[3] = 0x00 ;
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+void Motor::relayCLose()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xEB ;
+        _cmd[2] = 0x00 ;
+        _cmd[3] = 0x00 ;
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //左移
-void Motor::moveLeft()
+void Motor::moveLeft(double steps)
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[4] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE2 ;
+        steps = std::abs(steps);
+        for ( int i = 0; i < 2; ++i ) {
+            _cmd[3-i] = steps % 256 ;
+            steps >>= 8 ;
+        }
+        _cmd[4] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //右移
-void Motor::moveRight()
+void Motor::moveRight(double steps)
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[4] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE3 ;
+        steps = std::abs(steps);
+        for ( int i = 0; i < 2; ++i ) {
+            _cmd[3-i] = steps % 256 ;
+            steps >>= 8 ;
+        }
+        _cmd[4] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 
 //停止移动
-void Motor::stopMove()
+void Motor::stopMoveX()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE5 ;
+        _cmd[2] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
+//停止移动
+void Motor::stopMoveZ()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xEA ;
+        _cmd[2] = 0x00 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+
+void Motor::requestPosition()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xD1 ;
+        _cmd[2] = 0x00 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+
 //X位置反馈
 void Motor::getPositionX()
 {
@@ -64,48 +357,347 @@ void Motor::rightLimit()
 //报错恢复重置
 void Motor::reset()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[4] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0xA5 ;
+        _cmd[1] = 0xDD ;
+        _cmd[2] = 0xFF ;
+        _cmd[3] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //使能
-void Motor::enable()
+void Motor::enableX()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xDB ;
+        _cmd[2] = 0x01 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //去使能
-void Motor::disabled()
+void Motor::disabledX()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xDB ;
+        _cmd[2] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+
+//使能
+void Motor::enableZ()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xDC ;
+        _cmd[2] = 0x01 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
+}
+//去使能
+void Motor::disabledZ()
+{
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xDC ;
+        _cmd[2] = 0x00 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 
 
 //左移
 void Motor::moveUP()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[5] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE7 ;
+        steps = uint32_t(steps);
+        for ( int i = 0; i < 2; ++i ) {
+            _cmd[3-i] = steps % 256 ;
+            steps >>= 8 ;
+        }
+        _cmd[4] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //右移
 void Motor::moveDown()
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[5] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE8 ;
+        steps = uint32_t(steps);
+        for ( int i = 0; i < 2; ++i ) {
+            _cmd[4-i] = steps % 256 ;
+            steps >>= 8 ;
+        }
+        _cmd[4] = 0x00 ;
 
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //X绝对位移
-void Motor::absoluteMoveX(double)
+void Motor::absoluteMoveX(double steps)
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[6] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE4 ;
+        steps = uint32_t(steps);
+        for ( int i = 0; i < 3; ++i ) {
+            _cmd[4-i] = steps % 256 ;
+            steps >>= 8 ;
+        }
+        _cmd[5] = 0x00 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 
 }
 //Z绝对位移
 void Motor::absoluteMoveZ(double)
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[6] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0x5A ;
+        _cmd[1] = 0xE9 ;
+        steps = uint32_t(steps);
+        for ( int i = 0; i < 3; ++i ) {
+            _cmd[4-i] = steps % 256 ;
+            steps >>= 8 ;
+        }
+        _cmd[5] = 0x00 ;
+
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 
 }
 
 //下限位
-void Motor::DownLimit()
+void Motor::DownLimit(DownLimitOperate opt)
 {
-
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0xA5 ;
+        _cmd[1] = 0xD8 ;
+        if(opt == DownLimitOperate::TRIGGER)
+        {
+            _cmd[2] = 0x00 ;//00 触发
+        }else{
+            _cmd[2] = 0x01 ;//01 恢复
+        }
+        _cmd[3] = 0x00 ;
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 }
 //上限位
-void Motor::UPLimit()
+void Motor::UPLimit(UPLimitOperate opt)
 {
+    std::lock_guard<std::recursive_mutex> lck(_mtx);
+    if(!_serialPort_ptr)
+        return;
+    try{
+        unsigned char _cmd[3] ;
+        memset( _cmd, 0, sizeof(_cmd));
+        _cmd[0] = 0xA5 ;
+        _cmd[1] = 0xD7 ;
+        if(opt == UPLimitOperate::TRIGGER)
+        {
+            _cmd[2] = 0x00 ;//00 触发
+        }else{
+            _cmd[2] = 0x01 ;//01 恢复
+        }
+
+        _cmd[3] = 0x00 ;
+        for ( size_t i = 0; i < sizeof(_cmd); ++i ) {
+            printf ( "0X%X ", _cmd[i]) ;
+        }
+        printf( "\n" ) ;
+        boost::asio::write(*_serialPort_ptr, buffer(_cmd, sizeof(_cmd)));
+        _bReply = false ;
+        waitReply() ;
+        std::cout << "rotate over\n" << std::flush ;
+    }
+    catch(const boost::exception &e)
+    {
+        DUMPERROR(e) ;
+        _condReply.notify_all();
+    }
 
 }
--- weldroidProject/src/UI/XindeBao/motor/motor.h
+++ weldroidProject/src/UI/XindeBao/motor/motor.h
@@ -1,8 +1,22 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 #include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <thread>
+
+#include <boost/asio/serial_port.hpp>
+#include <boost/asio.hpp>
+#include <boost/timer.hpp>
 
 using namespace std;
+enum UPLimitOperate{TRIGGER,RELEASE};
+enum DownLimitOperate{TRIGGER,RELEASE};
+enum LeftLimitOperate{TRIGGER,RELEASE};
+enum RightLimitOperate{TRIGGER,RELEASE};
+enum LimitDirection{};
+enum MotorType{MOTORX,MOTORZ};
 
 class Motor
 {
@@ -13,27 +27,63 @@ public:
 public:
     void connectDevice();//连接设备
     void disconnectDevice();//断开设备
-    void backHome();//回Home位置
-    void moveUP();//上移
-    void moveDown();//下移
-    void moveLeft();//左移
-    void moveRight();//右移
-    void absoluteMoveX(double);//X绝对位移
-    void absoluteMoveZ(double);//Z绝对位移
-    void stopMove();//停止移动
+    void reconnectDevice();
+    void backHomeX();//回Home位置
+    void backHomeZ();//回Home位置
+    void relayOpen();
+    void relayCLose();
+    void moveUP(double steps);//上移
+    void moveDown(double steps);//下移
+    void moveLeft(double steps);//左移
+    void moveRight(double steps);//右移
+    void absoluteMoveX(double steps);//X绝对位移
+    void absoluteMoveZ(double steps);//Z绝对位移
+    void stopMoveX();//停止X移动
+    void stopMoveZ();//停止Z移动
+    void requestPosition();
     void getPositionX();//X位置反馈
     void getPositionZ();//Z位置反馈
     void bXHomeLost();//是否失去Home点
     void bZHomeLost();//是否失去Home点
-    void UPLimit();//上限位
-    void DownLimit();//下限位
-    void leftLimit();//左限位
-    void rightLimit();//右限位
+    void UPLimit(UPLimitOperate opt);//上限位
+    void DownLimit(DownLimitOperate opt);//下限位
+    void leftLimit(LeftLimitOperate opt);//左限位
+    void rightLimit(RightLimitOperate opt);//右限位
     void reset();//报错恢复重置
-    void enable();//使能
-    void disabled();//去使能
+    void enableX();//使能X
+    void disabledX();//去使能X
+    void enableZ();//使能Z
+    void disabledZ();//去使能Z
+
+    /// 存放命令
+    unsigned char _cmd[16] ;
+
+    /// 存放应答指令
+    unsigned char _readBuffer[16] ;
+
+    /// 线程同步锁
+    std::recursive_mutex _mtx ;
+    /// 条件变量
+    std::condition_variable _condReply ;
+    /// 条件变量锁
+    std::mutex _mtxReply ;
+    /// 是否已经应答
+    bool _bReply ;
+    /// asio 服务管理
+    boost::asio::io_service _service ;
+    /// 激光控制通信管理
+    std::shared_ptr<boost::asio::serial_port> _serialPort_ptr ;
+
+    bool _bOpened ;
+
+    std::thread _thService ;
 
 private:
+    /// 设备应答
+    void recvReply( const boost::system::error_code &ec, size_t ) ;
+    /// 等待设备应答
+    void waitReply() ;
+
 
 
 };
